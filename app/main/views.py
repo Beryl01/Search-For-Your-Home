4691b690ca2fef5fd75734c0d78581c1ef47ef5d
Nabz <bnnabiswa@gmail.com>
2020-05-13 12:46:30 +0300

    change category render template to categories.html

--- app/main/views.py
+++ app/main/views.py
@@ -5,6 +5,13 @@ from . import main
 @main.route("/")
 @main.route("/index")
 def index():
+
+    return render_template('index.html')
+   
+
+
+@main.route("/categories")
+def view_categories():
     all_properties = Property.query.all()
     nairobi_properties = Property.query.filter_by(location = 'nairobi').all() 
     nakuru_properties = Property.query.filter_by(location = 'nakuru').all()
@@ -16,4 +23,4 @@ def index():
     _40_to_60 = Property.query.filter_by(rent_category = '_40_to_60').all()
     above_60 = Property.query.filter_by(rent_category = 'above_60').all()
    
-    return render_template('index.html',all_properties = all_properties, nairobi_properties = nairobi_properties, mombasa_properties = mombasa_properties, nakuru_properties = nakuru_properties, kisumu_properties = kisumu_properties, eldoret_properties = eldoret_properties, _0_to_20 = _0_to_20, _20_to_40 = _20_to_40, _40_to_60 = _40_to_60, above_60 = above_60 )
+    return render_template('categories.html',all_properties = all_properties, nairobi_properties = nairobi_properties, mombasa_properties = mombasa_properties, nakuru_properties = nakuru_properties, kisumu_properties = kisumu_properties, eldoret_properties = eldoret_properties, _0_to_20 = _0_to_20, _20_to_40 = _20_to_40, _40_to_60 = _40_to_60, above_60 = above_60 )
